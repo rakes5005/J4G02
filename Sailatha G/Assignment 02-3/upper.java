874a92d880a5d1a17712475743c63b459c093ff8
7416961734 <101718518+7416961734@users.noreply.github.com>
2022-03-20 22:05:27 +0530

    Add files via upload

---
+++ Sailatha G/Assignment 02-3/upper.java
@@ -0,0 +1,11 @@
+package conversion;
+
+public class upper {
+        public static void main(String args[]) {
+        	String i1="this is sailatha";
+        	System.out.println(i1);
+        	System.out.println(i1.toLowerCase());
+        	System.out.println(i1.toUpperCase());
+        	
+        }
+}
