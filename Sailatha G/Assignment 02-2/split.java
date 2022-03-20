b79235077b75e9fbb97e3c46664950f579899769
7416961734 <101718518+7416961734@users.noreply.github.com>
2022-03-20 22:00:11 +0530

    Add files via upload

---
+++ Sailatha G/Assignment 02-2/split.java
@@ -0,0 +1,12 @@
+package split;
+
+public class split {
+	 public static void main(String[] args) {
+		 String str="hi sukku how are you";
+		 String split[]=str.split(" ",0);
+		 for(String s:split) {
+			 System.out.println(s);
+		 }
+	 }
+
+}
