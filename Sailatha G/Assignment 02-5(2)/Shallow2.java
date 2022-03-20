3e688141f933d81382dbc61f43d7f4ba1cae22ab
7416961734 <101718518+7416961734@users.noreply.github.com>
2022-03-20 22:24:16 +0530

    Add files via upload

---
+++ Sailatha G/Assignment 02-5(2)/Shallow2.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+public class Shallow2 {
+  
+	public static void main(String[] args) {
+		int[] vals= {3,7,9};
+		Shallow e = new Shallow(vals);
+		e.ShowData();
+		Vals[0]=13;
+		e.ShowData();
+
+	}
+
+}
