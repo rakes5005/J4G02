43c2294605b7de8410b53fd431206c8ea27d4c1e
7416961734 <101718518+7416961734@users.noreply.github.com>
2022-03-20 22:38:52 +0530

    Add files via upload

---
+++ Sailatha G/Assignment 03-1/ArithmeticException.java
@@ -0,0 +1,11 @@
+package assignment3;
+
+public class ArithmeticException {
+
+	public static void main(String[] atgs) {
+		int i =666;
+		int j =22;
+		double r=i/j;
+		System.out.println(r);
+	}
+}
