ba3c9ab21e41ba96c69289c24e0225594fddfbad
7416961734 <101718518+7416961734@users.noreply.github.com>
2022-03-20 22:21:33 +0530

    Add files via upload

---
+++ Sailatha G/Assignment 02-5(1)/Shallow1.java
@@ -0,0 +1,21 @@
+package Assignment2;
+
+import java.util.*;
+
+public class Shallow1 {
+	
+      private int[]data;
+	
+	public Shallow1(int[] values) {
+		data=values;
+	}
+	
+	public void ShowData() {
+		System.out.println(Arrays.toString(data));
+	}
+
+}
+
+	
+
+
