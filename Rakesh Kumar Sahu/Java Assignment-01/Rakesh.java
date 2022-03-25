8eb533375d006d38b49884b906f457667e0b0c1c
rakes5005 <101203136+rakes5005@users.noreply.github.com>
2022-03-25 10:36:02 +0530

    Add files via upload

---
+++ Rakesh Kumar Sahu/Java Assignment-01/Rakesh.java
@@ -0,0 +1,15 @@
+package module1;
+
+public class Rakesh {
+
+	
+	public static void myMethod() {
+	    System.out.println("My name is Rakesh");
+	  }
+
+		  public static void main(String[] args) {
+		    myMethod();
+		  }
+		
+
+}
