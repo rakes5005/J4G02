7793192c6ec95ddc27830ba10a331e5158b6dd11
rakes5005 <101203136+rakes5005@users.noreply.github.com>
2022-03-31 10:21:04 +0500

    Add files via upload

---
+++ Rakesh Kumar Sahu/Java Assignment-02/Employee.java
@@ -0,0 +1,29 @@
+package dateandtime;
+import java.time.Month;
+
+
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+
+public class Employee {
+	public static void main(String args[]) {
+		LocalDate EmployeeBirthday = LocalDate.of(1990,Month.APRIL,6);
+		System.out.println(EmployeeBirthday);
+		
+		
+		LocalDate nowDate = LocalDate.now();
+		System.out.println(nowDate);
+		
+		
+		
+		System.out.println(Period.between(EmployeeBirthday, nowDate));
+		System.out.println("Employeeage"+ChronoUnit.YEARS.between(EmployeeBirthday, nowDate));
+		
+		
+		
+	}
+
+}
